76343752714ad9eba14b52d01e671806affe5d32
Семенчикова Екатерина <semyonchikovakata@icloud.com>
2026-09-20 15:10:26 +0300

    feat: setup project structure and global styles from coursework

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export function App() {
+  return (
+    <div className="app-container">
+      {/* Здесь будут Header, Routes и Footer */}
+    </div>
+  );
+}
+
+export default App;
